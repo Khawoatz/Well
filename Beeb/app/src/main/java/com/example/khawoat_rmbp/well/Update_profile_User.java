eb5452a82746780dc3eef633923c9d705522fd75
jah <natta.jaw.it57@cpc.ac.th>
2018-03-10 00:17:30 +0700

    edok

--- Beeb/app/src/main/java/com/example/khawoat_rmbp/well/Update_profile_User.java
+++ Beeb/app/src/main/java/com/example/khawoat_rmbp/well/Update_profile_User.java
@@ -3,11 +3,11 @@ package com.example.khawoat_rmbp.well;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class activity_update_profile extends AppCompatActivity {
+public class Update_profile_User extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_profile);
+        setContentView(R.layout.activity_update_profile__user);
     }
 }
